817e6b304ab4f7576dddd352df65e7a153907c78
r25ta <twentyfive@hotmail.com>
2019-11-24 17:59:06 -0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'Um telefone grande com uma das melhores telas'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'Um ótimo telefone com uma das melhores câmeras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
