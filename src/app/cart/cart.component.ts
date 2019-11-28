5a7d010eb40ecdd5fc3a4547614ffe388c171a14
r25ta <twentyfive@hotmail.com>
2019-11-27 22:22:53 -0300

    Formulários - Implementação de formulário no projeto

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -7,15 +8,32 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  /*Propriedades / Atributos da classe */
   itens;
+  checkoutForm;
 
+  /* Inject dos componentes CartService e FormBuilder */
   constructor(
-    private cartService : CartService
+    private cartService : CartService,
+    private formBuilder : FormBuilder,
   
-  ) { }
-
-  ngOnInit() {
+  ) { 
     this.itens = this.cartService.getItens();
+    
+    /*Definindo os campos do formulario utilizando um grupo de propriedades */
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+
   }
 
+  ngOnInit() {
+  }
+
+  onSubmit(customerData){
+    console.warn('Seu pedido está sendo encaminhado',customerData);
+    this.itens = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
